d47415398e45da40cb5b16ce0ce8c338583bfe58
Fábio Sato <fabiosato@gmail.com>
2016-09-18 11:31:56 -0300

    Upgraded to latest clojure and instaparse stable versions

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://fabiosato.github.io/fabiosato-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-		 [instaparse "1.2.3"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+		 [instaparse "1.4.3"]]
 
   :main io.github.fabiosato.metar
 )
